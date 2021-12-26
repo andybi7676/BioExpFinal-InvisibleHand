f55a90d59172ca623a4c34617be10e7ba9a4db8d
Andy Tseng <59821107+andybi7676@users.noreply.github.com>
2021-12-27 00:43:28 +0800

    add data

--- sensor-arduino/right-hand/right-hand.ino
+++ sensor-arduino/right-hand/right-hand.ino
@@ -3,7 +3,7 @@
 #include <Math.h>
 #include <SoftwareSerial.h>
 SoftwareSerial Bluetooth(2, 3); //2 <-> Tx , 3 <-> Rx
-//SoftwareSerial LeftHand(6, 7); //8 <-> Tx, 9 <-> Rx
+//SoftwareSerial LeftHand(8, 9); //8 <-> Tx, 9 <-> Rx
 
 MPU6050 mpu6050(Wire);
 
@@ -14,16 +14,18 @@ float accZ = 0.0;
 float accMag = 0.0;
 char l = ' ';
 String l_signal = "";
+int leftHandAngles[3] = {0, 0, 0}; // index 0 -> x, index 1 -> y, index 2 -> z
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
   Bluetooth.begin(9600);
-  //LeftHand.begin(9600);
+//  LeftHand.begin(9600);
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
   while (Serial.available()) {
-    Serial.read();
+    l = Serial.read();
+    if (l == '\n') break;
   }
 }
 
@@ -37,20 +39,10 @@ void loop() {
     accZ = accZ / count;
     accMag = sqrt(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2));
     
-//    Serial.println("=======================================================");
-//    Serial.print("temp : ");Serial.println(mpu6050.getTemp());
-//    Serial.print("accX : ");Serial.print(accX);
-//    Serial.print("\taccY : ");Serial.print(accY);
-//    Serial.print("\taccZ : ");Serial.println(accZ);
-//    
-//    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
-//    Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
-//    Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
-//    Serial.println("=======================================================\n");
     String signalToSend = String((int)(accX*1000)) + " " + String((int)(accY*1000)) + " " + String((int)(accZ*1000)) + " " \
-                        + String((int)mpu6050.getAngleX()) + " " + String((int)mpu6050.getAngleY()) + " " + String((int)mpu6050.getAngleZ()) + "\n";
-    //Serial.print(signalToSend);
-//    Serial.println(accMag);
+                        + String((int)mpu6050.getAngleX()) + " " + String((int)mpu6050.getAngleY()) + " " + String((int)mpu6050.getAngleZ()) + " " \
+                        + String(leftHandAngles[0]) + " " + String(leftHandAngles[1]) + " " + String(leftHandAngles[2]) + "\n";
+    Serial.print(signalToSend);
     Bluetooth.print(signalToSend);
     timer = millis();
     accX = 0.0;
@@ -69,7 +61,20 @@ void loop() {
     l_signal += l;
     if (l == '\n') {
       Serial.print(l_signal);
+      parseLeftHandSignal();
       l_signal = "";
+    }
+  }
+}
+
+void parseLeftHandSignal() {
+  int start = 0;
+  int index = 0;
+  for (int e=0; e<l_signal.length(); ++e) {
+    if (l_signal[e] == ' ' || l_signal[e] == '\n') {
+      leftHandAngles[index] = l_signal.substring(start, e).toInt();
+      index++;
+      start = e+1;
     }
   }
 }
