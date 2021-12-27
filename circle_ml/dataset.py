d1ac226baa80040f46575f52998f1896ae390766
faliwang <faliwang1999@gmail.com>
2021-12-27 10:04:04 +0800

    Update dataset.py

--- circle_ml/dataset.py
+++ circle_ml/dataset.py
@@ -21,9 +21,9 @@ class CircleDataset(Dataset):
 
     def __getitem__(self, index):
         data_array, label = self.data[index]
-        data_array.dtype = 'float'
+        data_array.dtype = 'float32'
         data_array = torch.from_numpy(data_array)
-        return torch.tensor(data_array), torch.tensor(label)
+        return torch.FloatTensor(data_array), torch.IntTensor(label)
 
     def get_data(self, data_dir):
         data_list = []
