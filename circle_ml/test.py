e9011c1f401677d3a8d49c3f0f05fb7671859f16
faliwang <faliwang1999@gmail.com>
2021-12-27 10:35:34 +0800

    Update test.py

--- circle_ml/test.py
+++ circle_ml/test.py
@@ -59,7 +59,7 @@ def main(
     for data, labels in tqdm(dataloader):
         with torch.no_grad():
             data = data.to(device)
-            labels = labels.to(device)
+            labels = labels.cpu().numpy()
             preds = model(data)
             preds = preds.cpu().numpy()
             for pred, label in zip(preds, labels):
